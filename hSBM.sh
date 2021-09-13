f14b3e830412678bf7bd2391e6f30b4cc66f8013
curtis-murray <murrcurt09@gmail.com>
2021-09-13 22:00:28 +0930

    getting new dataO

--- hSBM.sh
+++ hSBM.sh
@@ -1,9 +1,9 @@
 #!/bin/bash -l
 #SBATCH -p batch                                                # partition (this is the queue your job will be added to)                                                    # number of nodes (no MPI, so we only use a single node)
 #SBATCH -N 1                                                    # number of nodes
-#SBATCH -n 48
-#SBATCH --time=0-10:00:00                                       # walltime allocation, which has the format (D-HH:MM:SS), here set to 1 hour
-#SBATCH --mem=32GB                                              # memory required per node (here set to 4 GB)
+#SBATCH -n 2 
+#SBATCH --time=0-01:00:00                                       # walltime allocation, which has the format (D-HH:MM:SS), here set to 1 hour
+#SBATCH --mem=8GB                                              # memory required per node (here set to 4 GB)
 
 # Notification configuration
 #SBATCH --mail-type=END                                         # Send a notification email when the job is done (=END)
